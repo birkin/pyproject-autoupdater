a0d84a02e6fbc65b80fed5b8774b3aea82afff24
Birkin James Diana <birkin.diana@gmail.com>
2025-06-13 20:01:59 -0400

    updates venv-path check.

--- lib/lib_common.py
+++ lib/lib_common.py
@@ -17,13 +17,34 @@ def determine_venv_paths(project_path: Path) -> tuple[Path, Path]:
 
     Essentially allows a subprocess.run() command to act as if it's running in an activated venv.
     """
-    venv_bin_path: Path = project_path.parent / 'env' / 'bin'
-    venv_bin_path = venv_bin_path.resolve()
-    venv_path: Path = project_path.parent / 'env'
-    venv_path = venv_path.resolve()
+    venv_bin_path: Path = project_path / '.venv' / 'bin'
+    venv_bin_path: Path = venv_bin_path.resolve()
+    venv_path: Path = project_path / '.venv'
+    venv_path: Path = venv_path.resolve()
     log.debug(f'venv_bin_path: ``{venv_bin_path}``')
     log.debug(f'venv_path: ``{venv_path}``')
     return (venv_bin_path, venv_path)
+
+
+# def determine_venv_paths(project_path: Path) -> tuple[Path, Path]:
+#     """
+#     Given a project path, returns the venv-bin-path and the venv-path.
+
+#     Used, by calling code, to build:
+#         local_scoped_env['PATH'] = f'{venv_bin_path}:{local_scoped_env["PATH"]}'  # prioritizes venv-path
+#         local_scoped_env['VIRTUAL_ENV'] = str(venv_path)
+#     ...for use in subprocess.run() calls like:
+#         subprocess.run(the_command, check=True, env=local_scoped_env)  # so all installs will go to the venv
+
+#     Essentially allows a subprocess.run() command to act as if it's running in an activated venv.
+#     """
+#     venv_bin_path: Path = project_path.parent / 'env' / 'bin'
+#     venv_bin_path = venv_bin_path.resolve()
+#     venv_path: Path = project_path.parent / 'env'
+#     venv_path = venv_path.resolve()
+#     log.debug(f'venv_bin_path: ``{venv_bin_path}``')
+#     log.debug(f'venv_path: ``{venv_path}``')
+#     return (venv_bin_path, venv_path)
 
 
 def run_command(command: list) -> tuple[bool, dict]:
